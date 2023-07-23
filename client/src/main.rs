6efd5a1efdb07b55130345e89613f53e92c5e1b5
Jan Ole Hüser <ole@example.com>
2023-07-23 21:40:38 +0200

    WIP: Call ShellExecuteW() from rust
    The goal is to run the C++ program as Admin.

--- client/src/main.rs
+++ client/src/main.rs
@@ -5,16 +5,41 @@ use std::{
     io::*,
 };
 
+use windows::Win32::UI::Shell::*;
+use windows::core::PCWSTR;
+
 fn main() {
     
     let pid = std::process::id().to_string();
-    let output = Command::new("C:\\Users\\ols3\\prog\\DuplicateFileHandleAsAdmin\\x64\\Debug\\DuplicateFileHandleAsAdmin.exe")
-        .args([&pid, ".\\moin.txt"])
-        .stdout(Stdio::piped())
-        .stderr(Stdio::inherit())
-        .output()
-        .expect("failed to execute process");
+    let prog_dup_file_handle_as_admin = "C:\\Users\\ols3\\prog\\DuplicateFileHandleAsAdmin\\x64\\Debug\\DuplicateFileHandleAsAdmin.exe";
+    //let output = Command::new(prog_dup_file_handle_as_admin)
+    //    .args([&pid, ".\\moin.txt"])
+    //    .stdout(Stdio::piped())
+    //    .stderr(Stdio::inherit())
+    //    .output()
+    //    .expect("failed to execute process");
+    //
+    //println!("status: {}", output.status);
+    //std::io::stdout().write_all(&output.stdout).unwrap();
 
-    println!("status: {}", output.status);
-    std::io::stdout().write_all(&output.stdout).unwrap();
+    {
+        use std::ffi::OsStr;
+        use std::iter::once;
+        use std::os::windows::ffi::OsStrExt;
+        use windows::Win32::Foundation::HWND;
+        use windows::Win32::UI::WindowsAndMessaging::SW_SHOW;
+        let runas: [u16; 6] = [0x72u16, 0x75u16, 0x6eu16, 0x61u16, 0x73u16, 0u16];
+        let prog : Vec<u16> = OsStr::new(prog_dup_file_handle_as_admin).encode_wide().chain(once(0u16)).collect();
+        let hwnd = HWND(0);
+        let shell_exec_result = unsafe {
+            ShellExecuteW(
+                hwnd,
+                PCWSTR::from_raw(runas.as_ptr()),
+                PCWSTR::from_raw(prog.as_ptr()),
+                PCWSTR::null(),
+                PCWSTR::null(),
+                SW_SHOW,
+            )
+        };
+    }
 }
